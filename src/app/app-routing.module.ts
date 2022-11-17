3e8cc2d0d04bffd4341a44603b06c3ecbc3435f9
Ivaylo Filipov <ivaylofilipov.72@gmail.com>
2022-11-17 22:56:02 +0200

    Added match result service, css, htm, logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MatchResultsComponent } from './components/results/match-results/match-results.component';
 import { TeamsComponent } from './components/teams/teams/teams.component';
 
 const routes: Routes = [
@@ -8,6 +9,10 @@ const routes: Routes = [
   {
     path: 'get-teams',
     component: TeamsComponent,
+  },
+  {
+    path: 'get-results',
+    component: MatchResultsComponent,
   },
 ];
 
---
+++ src/app/services/match-results.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { MatchResultsInterface } from '../interfaces/match-results';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MatchResultsService {
+  private readonly apiUrl = `${environment.apiUrl}teams`;
+
+  constructor(private http: HttpClient) { }
+
+  getMachResults(): Observable<MatchResultsInterface[]> {
+    return this.http.get<MatchResultsInterface[]>(`${this.apiUrl}/get-results`);
+  }
+}
